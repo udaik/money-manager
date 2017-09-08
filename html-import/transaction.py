d963b1b3743f886a67b214c69165c549589ab0db
Udai Kiran <s.udaikiran@gmail.com>
2017-09-09 00:18:16 +0530

    added initial files.

---
+++ html-import/transaction.py
@@ -0,0 +1,9 @@
+class transaction:
+
+    def __init__(self, date, where, amount, debit, credit, balance):
+        self.date = date
+        self.where = where
+        self.amount = amount
+        self.debit = debit
+        self.credit = credit
+        self.balance = balance
